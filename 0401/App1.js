ea963350044e567ed8503d8b8f961f339d66d810
C108156111 <C108156111@nkust.edu.tw>
2021-04-01 12:27:13 +0800

    0401

---
+++ 0401/App1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = () => (
+    <div>
+   <h1>Who lives in my Garage?</h1>
+    </div>
+);
+
+export default App;
---
+++ 0401/App2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Car(){
+    return (
+        <h1>Who lives in my Garage?</h1>
+    );
+}
+export default Car;

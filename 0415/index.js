f951743233f216e1cbf759d3b43194a8d3443b83
C108156111 <C108156111@nkust.edu.tw>
2021-04-15 11:48:47 +0800

    04151

---
+++ 0415/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock from './App6.js';
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
